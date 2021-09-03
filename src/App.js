05775b0beddc06518a573b5afe8e422de47d23d8
izidormaklary <maklaryizidor@gmail.com>
2021-09-03 12:30:03 +0200

    querying inside of a component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import ExchangeRates from "./components/ExchangeRates";
 
 function App() {
   return (
       <div>
         <h2>My first Apollo app 🚀</h2>
+          <ExchangeRates/>
       </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql} from "@apollo/client";
+import {ApolloClient, InMemoryCache, ApolloProvider} from "@apollo/client";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,14 +9,7 @@ const client = new ApolloClient({
     uri: "https://48p1r2roz4.sse.codesandbox.io",
     cache: new InMemoryCache()
 });
-const EXCHANGE_RATES = gql`
-            query GetExchangeRates{
-                rates(currency: "USD"){
-                    currency
-                    rate
-                }
-            }
-        `;
+
 
 
 render(
@@ -25,20 +18,10 @@ render(
     </ApolloProvider>,
     document.getElementById('root'),
 )
-// client
-//     .query({
-//         query: gql`
-//             query GetRates{
-//                 rates(currency: "USD"){
-//                     currency
-//                 }
-//             }
-//         `
-//     })
-//     .then(result => console.log(result));
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
 
